8fd7412cb83a8bbd73376c0b4b2af27298864ed2
seanlundberg4-create <seanlundberg4@gmail.com>
2026-09-18 16:12:45 +0500

    feat: replace all categories with Fashion News, Fashion Trends, Celebrity, Designers And Brands, Beauty, How to Style

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ import { LookbookDrawer } from './components/LookbookDrawer';
 import { BookmarksDrawer } from './components/BookmarksDrawer';
 import { Footer } from './components/Footer';
 
-const STORAGE_KEY_ARTICLES = 'fashiongraviti_articles_v1';
-const STORAGE_KEY_BOOKMARKS = 'fashiongraviti_bookmarks_v1';
+const STORAGE_KEY_ARTICLES = 'fashiongraviti_articles_v2';
+const STORAGE_KEY_BOOKMARKS = 'fashiongraviti_bookmarks_v2';
 const STORAGE_KEY_THEME = 'fashiongraviti_theme_v1';
 
 export function App() {
